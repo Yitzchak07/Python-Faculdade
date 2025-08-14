1f29d2c8be6fdbaa0714e8560983145a3d007051
Isac <157946284+Yitzchak07@users.noreply.github.com>
2025-08-14 17:27:19 -0300

    atualização exercicio de Funções
    update function

---
+++ unidade_1/Exercicio_funcao.py
@@ -0,0 +1,30 @@
+def media_aluno():
+    while True:
+        media = 0
+
+        nota1 = int(input(f"digite a nota do aluno "))
+        nota2 = int(input(f"digite a nota do aluno "))
+        nota3 = int(input(f"digite a nota do aluno "))
+        nota4 = int(input(f"digite a nota do aluno "))
+            
+        media = (nota1 + nota2 + nota3 + nota4) / 4 
+        if media >= 6:
+            print("Aluno aprovado")
+        elif media >= 4 and media <= 5:
+            print("recuperancao")
+        else:
+            print("reprovado")
+
+        print("deseja Continuar S/N:")
+        resp = str(input("Digite sua resposta:")).upper().strip()
+        if resp == "S":
+            print(f"media dos aluno foi {media} ")
+            
+            continue
+        else:
+            print(f"media dos aluno foi {media} ")
+            break
+    
+
+media_aluno()
+    
